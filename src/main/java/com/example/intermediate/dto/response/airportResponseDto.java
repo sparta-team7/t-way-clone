861395c9a7d541184983223967b9b51eaa94b624
yonykk <gwecs626@gmail.com>
2022-09-09 20:40:44 +0900

    ticket update

---
+++ src/main/java/com/example/intermediate/dto/response/airportResponseDto.java
@@ -0,0 +1,27 @@
+package com.example.intermediate.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Ticket 상세 조회용 DTO
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class airportResponseDto {
+  private Long id;
+  private String arrivalCity;
+  private String startCity;
+  private String domesticStartTime;
+  private String domesticArrivalTime;
+  private String time;
+  private int domesticNum;
+  private int charge;
+
+  private List<PassengerResponseDto> passengerList = new ArrayList<>();
+}
